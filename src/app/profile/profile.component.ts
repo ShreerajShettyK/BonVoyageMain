0d2491bebc0589a2217c91667413c35586e2a093
vagadeeshwar <vagadeeshwar@gmail.com>
2024-04-17 13:07:08 +0530

    c

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,23 +1,22 @@
 // profile.component.ts
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import User  from 'firebase/compat/app';
+import User from 'firebase/compat/app';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
-  user!:any;
+  user!: any;
 
   constructor(private afAuth: AngularFireAuth) {}
 
   ngOnInit() {
-    this.afAuth.authState.subscribe(user => {
+    this.afAuth.authState.subscribe((user) => {
       if (user) {
         this.user = user;
-        console.log(user);
       }
     });
   }
